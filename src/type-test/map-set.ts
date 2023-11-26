f6d334d943cb6076436238761feee5aa6488004b
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2023-11-27 00:30:57 +0900

    map and set

---
+++ src/type-test/map-set.ts
@@ -0,0 +1,49 @@
+const map: Map<string,number> = new Map()
+map.set('key2',210)
+map.set('key1',1234)
+map.set('key3',600)
+
+const keyVar = 'key1'
+
+console.log(map.has(keyVar))
+console.log(map.get(keyVar))
+
+console.log(map.has('not existed key'))
+console.log(map.get('not existed key'))
+map.delete(keyVar)
+console.log(map)
+
+console.log(map.keys())
+
+console.log(map.values())
+
+console.log(map.entries())
+
+for (let entry of map.entries()) {
+    console.log(entry[0], entry[1]);
+}
+
+for (let [key, value] of map.entries()) {
+    console.log(key, value);
+}
+
+
+let entriesArray = Array.from(map.entries());
+console.log(Array.from(map.entries()))
+
+const set: Set<string> = new Set()
+set.add("set")
+set.add('set2')
+set.add('set deleted')
+
+set.delete('set deleted')
+
+console.log(set)
+console.log(set.has('set'))
+console.log(set.has('set deleted'))
+
+
+//WeakMap や WeakSetというものも存在する。
+//keys()やvalues(),entries()を持たないMap, Setであり、キーとしてはオブジェクトしか許されない。
+//また、キーのオブジェクトに対する参照が弱参照であり、キーとなるオブジェクトが他の箇所で使用されない場合には
+//ガベージコレクションの対象となり、メモリ上から削除可能となる。
